ab83af0452c378641392641b11d162089a3fd1e0
jtrtj <jt.r@live.com>
2018-12-04 15:56:53 -0700

    Run migration for Project

--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :project_management_example, ProjectManagementExample.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "jtrtj",
+  password: "jtrtj",
   database: "project_management_example_dev",
   hostname: "localhost",
   pool_size: 10
